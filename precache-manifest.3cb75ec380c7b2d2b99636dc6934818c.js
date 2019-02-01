cd7db9b33ed24a31ab6d672c97ddb3afc506bc30
AntonAdigamov <AdigamovAntonAAA@gmail.com>
2019-02-01 21:13:11 +0100

    Updates

---
+++ precache-manifest.3cb75ec380c7b2d2b99636dc6934818c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "b2bb3043818105ddccd7",
+    "url": "/react-tutorial/static/css/main.adf89809.chunk.css"
+  },
+  {
+    "revision": "b2bb3043818105ddccd7",
+    "url": "/react-tutorial/static/js/main.b2bb3043.chunk.js"
+  },
+  {
+    "revision": "f4789f78bc5099d1ff6c",
+    "url": "/react-tutorial/static/js/1.f4789f78.chunk.js"
+  },
+  {
+    "revision": "d6d0aa42199878ab9859",
+    "url": "/react-tutorial/static/js/runtime~main.d6d0aa42.js"
+  },
+  {
+    "revision": "644ab14da04c652d696511e8e9ec36c8",
+    "url": "/react-tutorial/index.html"
+  }
+];
